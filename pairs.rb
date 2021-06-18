070a82e643bf0d93189aa32b4025d9b8ac7beabb
blinry <sebastian@morr.cc>
2021-06-18 09:49:37 +0200

    Write groups to a file, and avoid repetitions

--- pairs.rb
+++ pairs.rb
@@ -1,15 +1,31 @@
-size = 2
-people = %w(blinry Maren LF Jakob Mullana)
+size = 3
+people = %w(blinry Winston LF Pecca Hanno Winnie Lena karlabyrinth)
 
-people.shuffle!
+previous_groups =  IO.readlines("previous_groups").filter{|l| not l.empty?}.map { |line|
+    line.chomp.split(",").map(&:strip).sort
+}
 
-groups = people.each_slice(size).to_a
-p groups
+10000.times do |i|
+    people.shuffle!
+    groups = people.each_slice(size).to_a
 
-if groups.last.size == 1
-  groups[-2].concat groups.pop
+    if groups.last.size == 1
+      groups[-2].concat groups.pop
+    end
+    groups.map!(&:sort)
+
+    next unless previous_groups.intersection(groups).empty?
+
+    open("previous_groups", "a") do |f|
+        f.puts
+        puts "Found a grouping after #{i+1} attempts:"
+        groups.each_with_index do |g, i|
+          puts "Room #{i + 1}: #{g.join(", ")}"
+          f.puts g.join(", ")
+        end
+    end
+
+    exit # we are very happy here
 end
 
-groups.each_with_index do |g, i|
-  puts "Room #{i + 1}: #{g.join(", ")}"
-end
+puts "Didn't find new pairings for everyone"
