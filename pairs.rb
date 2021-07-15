958bb6f7d8788e96aa015a5ace943eb67d303d6b
blinry <mail@blinry.org>
2021-07-15 22:40:11 +0200

    Improve the program thanks to the very smart style robot. Thanks!

--- pairs.rb
+++ pairs.rb
@@ -1,44 +1,41 @@
-def has_no_repetitions groups, previous_groups
-    groups.each do |g|
-        previous_groups.each do |pg|
-            # It's bad if g is a subset of pg:
-            if g.difference(pg).empty?
-                return false
-            end
-        end
+# frozen_string_literal: true
+
+def repetitions?(groups, previous_groups)
+  groups.any? do |g|
+    previous_groups.any? do |pg|
+      # It's bad if g is a subset of pg:
+      g.difference(pg).empty?
     end
-    return true
+  end
 end
 
-size = 2
-people = %w(Edgar Jakob Paolo skalabyrinth hanno Piko Winnie Lena Leah mad Daniel_Bohrer mo blinry Lena_aus_Leipzig)
+size = 3
+people = %w[Edgar Jakob Paolo skalabyrinth hanno Piko Winnie Lena Leah mad Daniel_Bohrer mo blinry Lena_aus_Leipzig]
 
-previous_groups =  IO.readlines("previous_groups").filter{|l| not l.empty?}.map { |line|
-    line.chomp.split(",").map(&:strip).sort
-}
-
-100000.times do |i|
-    people.shuffle!
-    groups = people.each_slice(size).to_a
-
-    if groups.last.size == 1
-      groups[-2].concat groups.pop
-    end
-    groups.map!(&:sort)
-
-    # Try again if there's a duplicate.
-    next unless has_no_repetitions(groups, previous_groups)
-
-    open("previous_groups", "a") do |f|
-        f.puts
-        puts "Found a grouping after #{i+1} attempts:"
-        groups.each_with_index do |g, i|
-          puts "Room #{i + 1}: #{g.join(", ")}"
-          f.puts g.join(", ")
-        end
-    end
-
-    exit # we are very happy here
+previous_groups = IO.readlines('previous_groups').filter { |l| !l.empty? }.map do |line|
+  line.chomp.split(',').map(&:strip).sort
 end
 
-puts "Didn't find new pairings for everyone"
+100_000.times do |i|
+  people.shuffle!
+  groups = people.each_slice(size).to_a
+
+  groups[-2].concat groups.pop if groups.last.size == 1
+  groups.map!(&:sort)
+
+  # Try again if there's a duplicate.
+  next if repetitions?(groups, previous_groups)
+
+  open('previous_groups', 'a') do |f|
+    f.puts
+    puts "Found a grouping after #{i + 1} attempts:"
+    groups.each_with_index do |g, room_number|
+      puts "Room #{room_number + 1}: #{g.join(', ')}"
+      f.puts g.join(', ')
+    end
+  end
+
+  exit # we are very happy here
+end
+
+puts 'Didn\'t find new pairings for everyone'
